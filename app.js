4be603efd102f84095bd8dd21adc968a70a895d2
hmin01 <goals1003@gmail.com>
2020-09-21 08:07:03 +0900

    Add video list page add video list page and css, fontawesome

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/thumbnail', express.static(path.join(__dirname, '/public/images/thumbnail')));
 // Append router
 app.use('/', indexRouter);
 app.use('/user', userRouter);
