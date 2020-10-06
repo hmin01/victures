f1bc12b7fb89f8410f2f7919242c434c1134aad5
hmin01 <goals1003@gmail.com>
2020-10-06 18:23:22 +0900

    Modify video info UI and

---
+++ modules/video.js
@@ -0,0 +1,42 @@
+const url = require('url');
+const childProc = require('child_process');
+
+// Support host list
+const supportHost = [
+    'youtube.com',
+    'www.youtube.com'
+]
+
+module.exports = {
+    parseUrl: function(urlString) {
+        try {
+            const parse = url.parse(urlString);
+            console.log(parse);
+            if (parse.host === undefined || parse.host === null) {
+                return {result: false, message: "Invalid url (Please include http/https)"};
+            } else if (!supportHost.includes(parse.host)) {
+                return {result: false, message: "현재 Beta 버전에서는 Youtube 영상만 지원하고 있습니다."};
+            } else {
+                return {result: true, message: parse};
+            }
+        } catch (err) {
+            return {result: false, message: err.message};
+        }
+    },
+    import: function(urlString, callback) {
+        const parseResult = this.parseUrl(urlString);
+        if (parseResult.result) {
+            const command = `youtube-dl -J "${urlString}"`;
+            childProc.exec(command, (error, stdout, stderr) => {
+                if (error !== null) {
+                    console.error(error);
+                    callback({result: false, messaeg: error});
+                } else {
+                    callback({result: true, message: JSON.parse(stdout)});
+                }
+            });
+        } else {
+            callback(parseResult);
+        }
+    }
+};
--- routes/convert.js
+++ routes/convert.js
@@ -5,54 +5,75 @@ const video = require('../modules/video');
 
 /* Step 1 page */
 router.get('/step/import', (req, res) => {
-    res.render('convert/import');
+    let url = "";
+    // if exist url in session
+    if (req.session.video !== undefined && req.session.video.url !== undefined) {
+        url = req.session.video.url;
+    }
+    // Render
+    res.render('convert/import', {url: url});
 });
 
 router.post('/step/import', async (req, res) => {
     const url = decodeURIComponent(req.body.url);
-    video.import(url, function(result) {
-        if (result.result) {
-            const duration = result.message.duration;
-            const d_h = Math.floor(duration / 3600);
-            const d_m = Math.floor((duration % 3600) / 60);
-            const d_s = duration % 60;
+    // Analysis URL
+    const result = video.parseUrl(url);
+    if (result.result) {
+        req.session.video = {
+            url: url,
+            info: {}
+        };
+    }
+    // Return
+    res.json(result);
+});
 
-            const hour = d_h === 0 ? 0 : (d_h < 10 ? "0" + d_h : d_h);
-            const munite = d_m < 10 ? "0" + d_m : d_m;
-            const seconds = d_s < 10 ? "0" + d_s : d_s;
+/* Step 2 page */
+router.get('/step/info', (req, res) => {
+    if (req.session.video === undefined || req.session.video.url === undefined) {
+        res.redirect(401, '/convert/step/import');
+    } else {
+        res.render('convert/info');
+    }
+});
 
-            const uDate = result.message.upload_date;
-            req.session.video = {
-                info: {
+router.post('/step/info', (req, res) => {
+    if (req.session.video === undefined || req.session.video.url === undefined) {
+        res.json({result: false, message: "Invalid process flow"});
+    } else {
+        video.import(req.session.video.url, function(result) {
+            if (result.result) {
+                const duration = result.message.duration;
+                const d_h = Math.floor(duration / 3600);
+                const d_m = Math.floor((duration % 3600) / 60);
+                const d_s = duration % 60;
+    
+                const hour = d_h === 0 ? 0 : (d_h < 10 ? "0" + d_h : d_h);
+                const munite = d_m < 10 ? "0" + d_m : d_m;
+                const seconds = d_s < 10 ? "0" + d_s : d_s;
+    
+                const uDate = result.message.upload_date.toString();
+                req.session.video.info = {
                     id: result.message.id,
                     url: result.message.webpage_url,
                     title: result.message.title,
                     description: result.message.description,
                     author: result.message.uploader,
                     authorUrl: result.message.uploader_url,
-                    uplaodDate: `${uDate.slice(0, 4)}/${uDate.slice(4, 6)}/${uDate.slice(6)}}`,
+                    uploadDate: `${uDate.slice(0, 4)}/${uDate.slice(4, 6)}/${uDate.slice(6)}`,
                     thumbnail: result.message.thumbnails[result.message.thumbnails.length - 1],
                     categories: result.message.categories,
                     tags: result.message.tags,
                     viewCount: result.message.view_count,
                     averageRating: result.message.average_rating,
-                    duration: hour === 0 ? `${munite}분 ${seconds}초` : `${hour}시 ${munite}분 ${seconds}초`
-                }
-            };
-            res.json({result: true});
-        } else {
-            res.json(result);
-        }
-    });
-});
-
-/* Step 2 page */
-router.get('/step/info', (req, res) => {
-    if (req.session.video === undefined || req.session.video.info === undefined) {
-        res.redirect(401, '/convert/step/import');
-    } else {
-        console.log(req.session.video.info.thumbnail);
-        res.render('convert/info', {info: req.session.video.info});
+                    duration: result.message.duration,
+                    durationString: hour === 0 ? `${munite}분 ${seconds}초` : `${hour}시 ${munite}분 ${seconds}초`
+                };
+                res.json({result: true, message: req.session.video.info});
+            } else {
+                res.json(result);
+            }
+        });
     }
 });
 
