5d0893f8688504b5aa39ef6d5f1194fb50a4996e
hmin01 <goals1003@gmail.com>
2020-10-07 13:48:22 +0900

    Modify video info UI

--- modules/video.js
+++ modules/video.js
@@ -28,10 +28,37 @@ module.exports = {
             const command = `youtube-dl -J "${urlString}"`;
             childProc.exec(command, (error, stdout, stderr) => {
                 if (error !== null) {
-                    console.error(error);
-                    callback({result: false, messaeg: error});
+                    console.error(error.message);
+                    callback({result: false, message: error.message});
                 } else {
-                    callback({result: true, message: JSON.parse(stdout)});
+                    // Save video info
+                    const videoInfo = JSON.parse(stdout);
+                    // Get subtitle list
+                    const command = `youtube-dl --list-subs "${urlString}"`;
+                    childProc.exec(command, (error, stdout, stderr) => {
+                        if (error !== null) {
+                            console.error(error.message);
+                            callback({result: false, message: error.message});
+                        } else {
+                            // Search available subtitles
+                            const allSubtitls = stdout.split(`Available subtitles for ${videoInfo.id}:\nLanguage formats\n`);
+                            if (allSubtitls.length !== 2) {
+                                callback({result: true, videoInfo: videoInfo, subtitls: []});
+                            } else {
+                                // Extract available subtitle list
+                                const rows = allSubtitls[1].split('\n');
+                                const availableSubtitles = [];
+                                for (const elem of rows) {
+                                    if (elem !== "") {
+                                        const rowSplit = elem.split('vtt, ttml');
+                                        availableSubtitles.push(rowSplit[0].trim());
+                                    }
+                                }
+                                callback({result: true, videoInfo: videoInfo, subtitls: availableSubtitles});
+                            }
+                            
+                        }
+                    });
                 }
             });
         } else {
--- routes/convert.js
+++ routes/convert.js
@@ -20,8 +20,7 @@ router.post('/step/import', async (req, res) => {
     const result = video.parseUrl(url);
     if (result.result) {
         req.session.video = {
-            url: url,
-            info: {}
+            url: url
         };
     }
     // Return
@@ -41,39 +40,46 @@ router.post('/step/info', (req, res) => {
     if (req.session.video === undefined || req.session.video.url === undefined) {
         res.json({result: false, message: "Invalid process flow"});
     } else {
-        video.import(req.session.video.url, function(result) {
-            if (result.result) {
-                const duration = result.message.duration;
-                const d_h = Math.floor(duration / 3600);
-                const d_m = Math.floor((duration % 3600) / 60);
-                const d_s = duration % 60;
-    
-                const hour = d_h === 0 ? 0 : (d_h < 10 ? "0" + d_h : d_h);
-                const munite = d_m < 10 ? "0" + d_m : d_m;
-                const seconds = d_s < 10 ? "0" + d_s : d_s;
-    
-                const uDate = result.message.upload_date.toString();
-                req.session.video.info = {
-                    id: result.message.id,
-                    url: result.message.webpage_url,
-                    title: result.message.title,
-                    description: result.message.description,
-                    author: result.message.uploader,
-                    authorUrl: result.message.uploader_url,
-                    uploadDate: `${uDate.slice(0, 4)}/${uDate.slice(4, 6)}/${uDate.slice(6)}`,
-                    thumbnail: result.message.thumbnails[result.message.thumbnails.length - 1],
-                    categories: result.message.categories,
-                    tags: result.message.tags,
-                    viewCount: result.message.view_count,
-                    averageRating: result.message.average_rating,
-                    duration: result.message.duration,
-                    durationString: hour === 0 ? `${munite}분 ${seconds}초` : `${hour}시 ${munite}분 ${seconds}초`
-                };
-                res.json({result: true, message: req.session.video.info});
-            } else {
-                res.json(result);
-            }
-        });
+        if (req.session.video.info === undefined || req.session.video.info === null) {
+            video.import(req.session.video.url, function(result) {
+                if (result.result) {
+                    // Calculate duration (convert string)
+                    const duration = result.videoInfo.duration;
+                    const d_h = Math.floor(duration / 3600);
+                    const d_m = Math.floor((duration % 3600) / 60);
+                    const d_s = duration % 60;
+                    const hour = d_h === 0 ? 0 : (d_h < 10 ? "0" + d_h : d_h);
+                    const munite = d_m < 10 ? "0" + d_m : d_m;
+                    const seconds = d_s < 10 ? "0" + d_s : d_s;
+                    // Upload date (convert format: YYYYMMDD -> YYYY-MM-DD)
+                    const rawUploadDate = result.videoInfo.upload_date.toString();
+                    const uploadDate = `${rawUploadDate.slice(0, 4)}-${rawUploadDate.slice(4, 6)}-${rawUploadDate.slice(6)}`;
+                    // Save video data in session
+                    req.session.video.info = {
+                        id: result.videoInfo.id,
+                        url: result.videoInfo.webpage_url,
+                        title: result.videoInfo.title,
+                        description: result.videoInfo.description,
+                        author: result.videoInfo.uploader,
+                        authorUrl: result.videoInfo.uploader_url,
+                        uploadDate: uploadDate,
+                        thumbnail: result.videoInfo.thumbnail,
+                        categories: result.videoInfo.categories,
+                        tags: result.videoInfo.tags,
+                        viewCount: result.videoInfo.view_count,
+                        averageRating: result.videoInfo.average_rating,
+                        duration: result.videoInfo.duration,
+                        durationString: hour === 0 ? `${munite}분 ${seconds}초` : `${hour}시 ${munite}분 ${seconds}초`,
+                        subtitls: result.subtitls
+                    };
+                    res.json({result: true, message: req.session.video.info});
+                } else {
+                    res.json(result);
+                }
+            });
+        } else {
+            res.json({result: true, message: req.session.video.info});
+        }
     }
 });
 
